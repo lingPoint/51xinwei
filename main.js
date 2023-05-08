d7321e73da7d6b650f5b17c386970b24a9cd2317
Zero <44696270+lingPoint@users.noreply.github.com>
2023-05-08 22:32:29 +0800

    Update main.js
    23/5/8 修复了遇到作业不能跳转下一集的问题。

--- main.js
+++ main.js
@@ -1,61 +1,46 @@
 // ==UserScript==
 // @name         芯位教育全自动刷网课
 // @namespace    https://github.com/lingPoint/51xinwei
-// @version      1.2.0
-// @description  自动下一节
+// @version      1.3.0
+// @description  自动下一集，修复了遇到文档不能跳转下一集的问题。
 // @author       Zero.
 // @match        https://teaching.51xinwei.com/*
 // @icon         https://teaching.51xinwei.com/*
 // @grant        none
 // @run-at document-end
-// @license AGPL-3.0 license
+// @license GPL-3.0 license
 // ==/UserScript==
 
 (function () {
-	'use strict';
+    'use strict';
     window.alert("脚本加载成功！");
+    const doc2 = document;
+    const cscs = doc2.querySelector('body');
+    let observer = new MutationObserver(handler);
+    const options = { childList: true }
+    observer.observe(cscs, options)
 })();
 
-const doc2 = document;
-const cscs = doc2.querySelector('body');
-let observer = new MutationObserver(handler);
-const options = { childList: true }
-observer.observe(cscs, options)
 function handler(mutationRecordList) {
-	//console.log(mutationRecordList);
-	main()
+    main()
 }
 
 function main() {
-	let menu_tarr_content = document.querySelectorAll('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope')
-	if (menu_tarr_content.length >= 2) {
-		let menu_tarr_content_active = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active').innerText
-		for (let i = 0; i < menu_tarr_content.length; i++) {
-			if (menu_tarr_content_active == menu_tarr_content[i].innerText) {
-				if (document.querySelector('.layui-layer-title') == null) {
-					wdtz()
-				}
-			}
-			dianji()
-		}
-	}
+    const activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active').innerText;
+    if (activeMenu === ' 文档' || activeMenu === ' 作业') {
+        jumpToNext();
+    } else if (document.querySelector('.layui-layer-title') !== null) {
+        document.querySelectorAll('.layui-layer-btn0')[0].click();
+    }
 }
 
-function dianji() {
-	if (document.querySelector('.layui-layer-title') != null) {
-		document.querySelectorAll('.layui-layer-btn0')[0].click();
-	}
-}
-
-function wdtz() {
-	let course_chapter_item = document.querySelectorAll('.course_chapter_item.user-no-select.ng-scope')
-	let course = document.querySelector('.course_chapter_item.user-no-select.ng-scope.active')
-
-	let activeItemText = course.innerText
-	for (let i = 0; i < course_chapter_item.length; i++) {
-		if (activeItemText == course_chapter_item[i].innerText) {
-			course_chapter_item[i + 1].children[1].click()
-			break;
-		}
-	}
+function jumpToNext() {
+    const courseChapterItems = document.querySelectorAll('.course_chapter_item.user-no-select.ng-scope');
+    const activeItemText = document.querySelector('.course_chapter_item.user-no-select.ng-scope.active').innerText;
+    for (let i = 0; i < courseChapterItems.length; i++) {
+        if (activeItemText === courseChapterItems[i].innerText) {
+            courseChapterItems[i + 1].children[1].click();
+            break;
+        }
+    }
 }
