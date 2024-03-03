0d637a785f226d167f24b3806d882a88812c74b1
Zero <44696270+lingPoint@users.noreply.github.com>
2024-03-03 16:29:46 +0800

    Update main.js
    24/3/3
    1、修复了遇到问卷不能跳转的问题。
    2、修复了DOM更新后不检测的问题。

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         芯位教育全自动刷网课
 // @namespace    https://github.com/lingPoint/51xinwei
-// @version      1.3.0
-// @description  自动下一集，修复了遇到文档不能跳转下一集的问题。
-// @author       Zero.
-// @match        https://teaching.51xinwei.com/*
-// @icon         https://teaching.51xinwei.com/*
+// @version      1.3.2
+// @description  主要功能：芯位教育自动播放下一课，跳过作业、文档、问卷；专注于帮助大学生从网课中释放出来 让自己的时间把握在自己的手中。
+// @author       Zerox
+// @match        *://*.51xinwei.com/*
+// @icon         *://*.51xinwei.com/*
 // @grant        none
 // @run-at document-end
 // @license GPL-3.0 license
@@ -13,7 +13,7 @@
 
 (function () {
     'use strict';
-    window.alert("脚本加载成功！");
+    window.alert("加载成功！");
     const doc2 = document;
     const cscs = doc2.querySelector('body');
     let observer = new MutationObserver(handler);
@@ -25,9 +25,13 @@ function handler(mutationRecordList) {
     main()
 }
 
+setInterval(function() {
+    main()
+}, 1000);
+
 function main() {
     const activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active').innerText;
-    if (activeMenu === ' 文档' || activeMenu === ' 作业') {
+    if (activeMenu === ' 文档' || activeMenu === ' 作业'|| activeMenu === ' 问卷') {
         jumpToNext();
     } else if (document.querySelector('.layui-layer-title') !== null) {
         document.querySelectorAll('.layui-layer-btn0')[0].click();
