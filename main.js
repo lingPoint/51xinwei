eb15c735c6b846af00beeb9185bb00814c73a5c0
Zero <44696270+lingPoint@users.noreply.github.com>
2024-04-03 22:25:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         芯位教育全自动刷网课
 // @namespace    https://github.com/lingPoint/51xinwei
-// @version      2.0.1
+// @version      2.0.2
 // @description  适配新版UI；主要功能：芯位教育自动播放下一课，跳过作业、文档、问卷；专注于帮助大学生从网课中释放出来 让自己的时间把握在自己的手中。
 // @author       Zerox
 // @match        *://*.51xinwei.com/*
@@ -30,10 +30,13 @@ setInterval(function () {
 }, 1000);
 
 function main() {
-    var activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active');
+    var activeMenutrim = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active');
 
-    if (activeMenu && activeMenu.innerText.trim() !== "") {
+    // 检查元素是否存在并且innerText属性是否有值
+    if (activeMenutrim && activeMenutrim.innerText.trim() !== "") {
+        // 存在且innerText有值
         console.log("老版界面");
+        const activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active').innerText;
         if (activeMenu === ' 文档' || activeMenu === ' 作业'|| activeMenu === ' 问卷') {
             jumpToNext();
         } else if (document.querySelector('.layui-layer-title') !== null) {
@@ -45,6 +48,7 @@ function main() {
             leftElements.forEach(function (element) {
                 element.click();
             });
+            // 不存在或innerText为空
             console.log("新版界面");
         }
 
