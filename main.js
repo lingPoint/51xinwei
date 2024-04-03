2d3e159fec26e0ea0ca9238874dec018b6cef7be
Zero <44696270+lingPoint@users.noreply.github.com>
2024-04-03 22:10:33 +0800

    Update main.js
    适配新版UI

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         芯位教育全自动刷网课
 // @namespace    https://github.com/lingPoint/51xinwei
-// @version      1.3.2
-// @description  主要功能：芯位教育自动播放下一课，跳过作业、文档、问卷；专注于帮助大学生从网课中释放出来 让自己的时间把握在自己的手中。
+// @version      2.0.1
+// @description  适配新版UI；主要功能：芯位教育自动播放下一课，跳过作业、文档、问卷；专注于帮助大学生从网课中释放出来 让自己的时间把握在自己的手中。
 // @author       Zerox
 // @match        *://*.51xinwei.com/*
 // @icon         *://*.51xinwei.com/*
@@ -13,7 +13,7 @@
 
 (function () {
     'use strict';
-    window.alert("加载成功！");
+    //window.alert("加载成功！");
     const doc2 = document;
     const cscs = doc2.querySelector('body');
     let observer = new MutationObserver(handler);
@@ -25,16 +25,29 @@ function handler(mutationRecordList) {
     main()
 }
 
-setInterval(function() {
+setInterval(function () {
     main()
 }, 1000);
 
 function main() {
-    const activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active').innerText;
-    if (activeMenu === ' 文档' || activeMenu === ' 作业'|| activeMenu === ' 问卷') {
-        jumpToNext();
-    } else if (document.querySelector('.layui-layer-title') !== null) {
-        document.querySelectorAll('.layui-layer-btn0')[0].click();
+    var activeMenu = document.querySelector('#menu_tarr_content .courseware_menu_item.pull-left.ng-scope.active');
+
+    if (activeMenu && activeMenu.innerText.trim() !== "") {
+        console.log("老版界面");
+        if (activeMenu === ' 文档' || activeMenu === ' 作业'|| activeMenu === ' 问卷') {
+            jumpToNext();
+        } else if (document.querySelector('.layui-layer-title') !== null) {
+            document.querySelectorAll('.layui-layer-btn0')[0].click();
+        }
+    } else {
+        if (document.querySelector('.left') !== null) {
+            var leftElements = document.querySelectorAll('.left');
+            leftElements.forEach(function (element) {
+                element.click();
+            });
+            console.log("新版界面");
+        }
+
     }
 }
 
